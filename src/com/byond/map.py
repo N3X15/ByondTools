53b93e21d84174f44bf14cf705b391fe6d7a8331
Rob Nelson <nexis@7chan.org>
2014-02-25 14:16:07 -0800

    Fix python3

--- src/com/byond/map.py
+++ src/com/byond/map.py
@@ -44,7 +44,7 @@ def BYOND2RGBA(colorstring, alpha=255):
     colorstring = colorstring.strip()
     if colorstring[0] == '#': colorstring = colorstring[1:]
     if len(colorstring) != 6:
-        raise ValueError, "input #%s is not in #RRGGBB format" % colorstring
+        raise ValueError("input #%s is not in #RRGGBB format" % colorstring)
     r, g, b = colorstring[:2], colorstring[2:4], colorstring[4:]
     r, g, b = [int(n, 16) for n in (r, g, b)]
     return (r, g, b, alpha)
