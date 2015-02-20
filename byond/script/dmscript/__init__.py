376d6cd5bae85360a68ae345e0840126dd018575
Rob Nelson <nexisentertainment@gmail.com>
2015-02-19 19:03:00 -0800

    More work on Dream.

--- byond/script/dmscript/__init__.py
+++ byond/script/dmscript/__init__.py
@@ -24,6 +24,26 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from byond.basetypes import BYONDFileRef, BYONDList, BYONDString, BYONDValue
 import pyparsing as pyp
 
+class PPStackElement(object):
+    def __init__(self, ends=[], toggles=[], blocking=False):
+        self.blocking = blocking
+        self.ends = ends
+        self.toggles = toggles
+        
+    def isBlocking(self):
+        return self.blocking
+    
+    def gotToken(self, name):
+        if name in self.ends:
+            return False # Pop off stack
+        if name in self.toggles:
+            self.blocking = not self.blocking
+        return True # continue
+    
+class IfDefElement(PPStackElement):
+    def __init__(self, state):
+        super(PPStackElement, self).__init__(self,ends=['endif'],toggles=['else'], blocking=state)
+    
 class DreamSyntax(object):
     def __init__(self, list_only=False, simplify_lists=False):
         if list_only:
@@ -33,7 +53,15 @@ class DreamSyntax(object):
         
         self.simplify_lists=simplify_lists
         
-    def ParseString(self, string):
+        #: Preprocessor defines.
+        self.macros = {}
+        
+        #: Current #ifdef stack. (PPStackElement)
+        self.ifstack = []
+        
+        self.atomContext = []
+        
+    def ParseString(self, filename):
         try:
             return self.syntax.parseString(string)
         except pyp.ParseException, err:
@@ -45,11 +73,83 @@ class DreamSyntax(object):
         '''WIP'''
         dreamScript = pyp.Forward()
         
-        # Literals
-        singlelineString = pyp.QuotedString('"','\\').setResultsName('string').setParseAction(self.makeListString)
+        # Constants
+        singlelineString = pyp.QuotedString('"','\\').setResultsName('string').setParseAction(self.makeString)
         fileRef = pyp.QuotedString("'",'\\').setResultsName('fileRef').setParseAction(self.makeFileRef)
-        multilineString = pyp.QuotedString(quoteChar='{"',endQuoteChar='"}',multiline=True).setResultsName('string').setParseAction(self.makeListString)
-        number = pyp.Regex(r'\d+(\.\d*)?([eE]\d+)?').setResultsName('number').setParseAction(self.makeListNumber)
+        multilineString = pyp.QuotedString(quoteChar='{"',endQuoteChar='"}',multiline=True).setResultsName('string').setParseAction(self.makeString)
+        number = pyp.Regex(r'\d+(\.\d*)?([eE]\d+)?').setResultsName('number').setParseAction(self.makeNumber)
+        
+        # Other symbols
+        listStart = pyp.Suppress('list(')
+        listEnd = pyp.Suppress(')')
+        
+        VAR_GLOBAL = pyp.Keyword('global')
+        VAR_CONST = pyp.Keyword('const')
+        SLASH = pyp.Literal('/')
+        EQUAL = pyp.Literal('=')
+        VAR = pyp.Keyword('var')
+        
+        #############################
+        # Grammar
+        #############################
+        
+        constant = singlelineString | fileRef | multilineString | number | dreamList
+        
+        #  Lists
+        listElement = constant | (constant + EQUAL + constant)
+        listElement = pyp.operatorPrecedence(listElement, [
+                                ("=", 2, pyp.opAssoc.LEFT,),
+                                ])
+        listContents = pyp.delimitedList(listElement)
+        dreamList << pyp.Group(listStart + listContents + listEnd)
+        dreamList.setParseAction(self.handleList)
+        
+        #  Paths
+        
+        relpath = pyp.ident | relpath + SLASH + pyp.ident
+        abspath = SLASH + relpath
+        path = (abspath | relpath).setParseAction(self.handlePath)
+        pathslash = path + SLASH
+        
+        #  Preprocessor stuff
+        ppStatement = pyp.Forward()
+        
+        ppDefine = pyp.Keyword('#define') + pyp.ident.setResultsName('name') + pyp.restOfLine.setResultsName('value')
+        ppDefine.setParseAction(self.handlePPDefine)
+        ppUndef = pyp.Keyword('#undef') + pyp.ident.setResultsName('name')
+        ppUndef.setParseAction(self.handlePPUndef)
+        
+        ppIfdef = (pyp.Keyword('#ifdef') + pyp.ident.setResultsName('name')).setParseAction(self.handlePPIfdef)
+        ppIfndef = (pyp.Keyword('#ifndef') + pyp.ident.setResultsName('name')).setParseAction(self.handlePPIfndef)
+        ppElse = (pyp.Keyword('#else') + pyp.ident.setResultsName('name')).setParseAction(self.handlePPElse)
+        ppEndif = pyp.Keyword('#endif').setParseAction(self.handlePPElse)
+        
+        ppStatement = pyp.lineStart + (ppIfdef | ppIfndef | ppElse | ppEndif)
+        
+        # Var Declarations
+        ##########################
+        var_modifiers = pyp.ZeroOrMore(SLASH + (VAR_GLOBAL|VAR_CONST)).setResultsName('modifiers')
+        var_assignment = EQUAL + constant
+        varblock_inner_ref = pyp.ident.setResultsName('name') + pyp.Optional(var_assignment)
+        var_argument = VAR + pyp.Optional(abspath) + SLASH +  pyp.ident.setResultsName('name') + pyp.Optional(var_assignment)
+        varblock_inner_decl = var_modifiers +  pyp.Optional(abspath) + SLASH + varblock_inner_ref
+        varblock_element = varblock_inner_decl | varblock_inner_ref
+        varblock = VAR + pyp.indentedBlock(var_blockinner_decl)
+        inline_vardecl = VAR + varblock_inner_decl
+        vardecl = varblock | inline_vardecl
+        
+        
+        
+        # Proc Declarations
+        PROC = pyp.Keyword('proc')
+        proc_args = '(' + pyp.delimitedList(var_argument | pyp.ident.setResultsName('name')) + ')'
+        procblock_proc=pyp.ident.setResultsName('name') + proc_args + pyp.indentedBlock(proc_instructions)
+        procblock = PROC + pyp.indentedBlock(procblock_proc, [1])
+        
+        # Atom blocks
+        atomdecl = pyp.Forward()
+        atomdecl << path + pyp.indentedBlock(vardecl | atomdecl | procdecl , [1])
+        
         
         return dreamScript
         
@@ -68,7 +168,7 @@ class DreamSyntax(object):
         
         # Grammar
         listConstant = singlelineString | fileRef | multilineString | number | dreamList
-        listElement = listConstant | (listConstant + pyp.Literal('=') + listConstant)
+        listElement = listConstant | (listConstant + '=' + listConstant)
         listElement = pyp.operatorPrecedence(listElement, [
                                 ("=", 2, pyp.opAssoc.LEFT,),
                                 ])
