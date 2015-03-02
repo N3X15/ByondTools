13035697650252583093e94a712811596353d57f
Rob Nelson <nexisentertainment@gmail.com>
2015-03-02 11:47:53 -0800

    Fix Atom unit tests. (They passed locally, somehow)

--- tests/Atom.py
+++ tests/Atom.py
@@ -9,10 +9,10 @@ class AtomTest(unittest.TestCase):
     def test_copy_consistency(self):
         from byond.basetypes import Atom, BYONDString, BYONDValue
         atom = Atom('/datum/test',__file__,0)
-        atom.properties={
-            'dir': BYONDValue(2),
-            'name': BYONDString('test datum')
-        }
+        
+        atom.properties['dir']=BYONDValue(2)
+        atom.properties['name']=BYONDString('test datum')
+        
         atom.mapSpecified=['dir','name']
         
         atom2=atom.copy()
@@ -25,13 +25,15 @@ class AtomTest(unittest.TestCase):
     def test_serialization(self):
         from byond.basetypes import Atom, BYONDString, BYONDValue
         atom = Atom('/datum/test',__file__,0)
-        atom.properties={
-            'dir': BYONDValue(2),
-            'name': BYONDString('test datum')
-        }
         
-        atom_serialized='/datum/test{name="test datum";dir=2}'
+        # Assign properties.  Order is important (orderedDict!)
+        atom.properties['dir']=BYONDValue(2)
+        atom.properties['name']=BYONDString('test datum')
         
+        # What we expect after running str().
+        atom_serialized='/datum/test{dir=2;name="test datum"}'
+        
+        # Check it
         self.assertEqual(str(atom), atom_serialized)
 
 if __name__ == "__main__":
